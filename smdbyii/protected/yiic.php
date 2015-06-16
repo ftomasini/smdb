fd1219f0ff435300010d2fa1dad270044a97a7e3
root <root@solis-PW-945GCX.(none)>
2015-06-16 13:50:41 -0300

    bibliotecas

---
+++ smdbyii/protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../../home/solis/Área de Trabalho/PROJETO TCC/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
