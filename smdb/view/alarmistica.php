1fdf0f601ba6c323874107f35c82a2b757417843
Fabiano Tomasini <ftomasini.rs@gmail.com>
2015-11-02 17:35:26 -0200

    Atualização documentos

--- smdb/view/alarmistica.php
+++ smdb/view/alarmistica.php
@@ -27,11 +27,12 @@
                         <h3 class="box-title">Melhor horário para manutenção</h3>
                         <div class="box-tools pull-right">
                             <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                            <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+                            <!--<button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+                     -->
                         </div>
                     </div>
                     <div class="box-body">
-                    De acordo com os logs do último dia (12/10/2015) o melhor horário para efetuar alguma manutenção é 00:42:29
+                    De acordo com as informações coletadas no último dia (12/10/2015) o melhor horário para efetuar alguma manutenção é 00:42:29
                     Load avarege: 0.2
                     </div><!-- /.box-body -->
                     <div class="box-footer">
