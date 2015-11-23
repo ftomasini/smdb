c4552abe61c695376e1d24649a2e1896f3919350
Fabiano Tomasini <ftomasini.rs@gmail.com>
2015-11-23 10:41:18 -0200

    Atualização documentos

---
+++ smdb/coletor/boot.php
@@ -0,0 +1,22 @@
+<?php
+
+while(true)
+{
+    $usuarios[] = 'fametro@fametro.com.br';
+    $usuarios[] = 'unifor@unifor.com.br';
+    $usuarios[] = 'smbd@smbd.com.br';
+
+    foreach ($usuarios as $usuario)
+    {
+        if(isset($usuario))
+        {
+            Estatistica::informacoesBaseDeDadosChart($usuario, '');
+            Estatistica::tamanhoBaseDeDadosChart($usuario, '');
+            Estatistica::tamanhoBaseDeDadosComIndicesChart($usuario, '');
+            Estatistica::aproveitamentoCacheBaseDeDadosChart($usuario, '');
+            Estatistica::tabelasComPoucaPesquisaPorIndicesChart($usuario, '');
+            Estatistica::consultasLentasChart($usuario, '');
+        }
+    }
+}
+?>
